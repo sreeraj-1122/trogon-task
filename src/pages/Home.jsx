268730f791178ba96ebb059076ba8b3cf4100c58
sreeraj <sreeraj2122@gmail.com>
2025-02-20 18:53:13 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import SubjectCard from "../components/SubjectCard";
 import { getSubjects } from "../services/api";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   const [subjects, setSubjects] = useState([]);
@@ -23,21 +23,35 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold text-gray-700 mb-6 text-center">Subjects</h1>
+    <div className="p-6 bg-purple-700 min-h-screen text-white">
+      <h1 className="text-3xl font-bold text-center mb-6">Subjects</h1>
 
       {loading && (
         <div className="flex justify-center items-center h-32">
-          <div className="animate-spin rounded-full h-10 w-10 border-4 border-blue-500 border-t-transparent"></div>
+          <div className="animate-spin rounded-full h-10 w-10 border-4 border-white border-t-transparent"></div>
         </div>
       )}
 
-      {error && <p className="text-red-500 text-center">{error}</p>}
+      {error && <p className="text-red-400 text-center">{error}</p>}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {subjects.map((subject) => (
-          <Link key={subject.id} to={`/subjects/${subject.id}`}>
-            <SubjectCard subject={subject} />
+      <div className="relative mt-6 p-6">
+        {subjects.map((subject, index) => (
+          <Link key={subject.id} to={`/subjects/${subject.id}`} className="flex items-center  relative mb-6 gap-6 p-3">
+            
+            {index !== subjects.length - 1 && (
+              <div className="absolute left-[35px] top-12 h-full w-[2px] bg-gray-300"></div>
+            )}
+
+            <div className="relative z-10 w-12 h-12 bg-white text-purple-700 font-bold flex items-center justify-center rounded-full border-4 border-purple-400 ">
+              {index + 1}
+            </div>
+
+            <div className="flex-1">
+              <p className="font-semibold">{subject.title}</p>
+              <p className="text-sm text-gray-300">{15} Lessons</p>
+            </div>
+
+            <FaArrowRight className="text-white" />
           </Link>
         ))}
       </div>
